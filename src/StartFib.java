0d363542d991b15b547693963b5bff587fbb31b3
ad1gan <adrian.kolb@rwth-aachen.de>
2016-04-17 20:50:50 +0200

    # WARNING: head commit changed in the meantime
    Neuer Test
    
    Change-Id: I96b2990937dc4c0f2119069397a8b9f98da25634
    Signed-off-by: ad1gan <adrian.kolb@rwth-aachen.de>

--- src/StartFib.java
+++ src/StartFib.java
@@ -4,8 +4,8 @@ public class StartFib extends Fibonacci{
 	public static void main(String args[])
 	{
 		int n = Integer.parseInt(args[0]);
-		System.out.println(recfib(n));
-		System.out.println(itfib(n));
+		System.out.println("Rekursiv: " + recfib(n));
+		System.out.println("Iterativ: " + itfib(n));
 	}
 
 }
