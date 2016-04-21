1fbcd9cd667d9671d932dcd1cc4074c3a930a52a
ad1gan <adrian.kolb@rwth-aachen.de>
2016-04-21 11:28:45 +0200

    Testlauf über Terminal

---
+++ StartFib.java
@@ -0,0 +1,13 @@
+
+public class StartFib extends Fibonacci{
+	
+	public static void main(String args[])
+	{
+		int n = Integer.parseInt(args[0]);
+		System.out.println("Rekursiv: " + recfib(n));
+		System.out.println("Iterativ: " + itfib(n));
+	}
+
+}
+
+// Test
